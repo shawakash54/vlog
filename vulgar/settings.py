8d36be39517d55c8d1830164f7a9178496c8b6d3
shawakash54 <akashshaw@ACKO398.local>
2020-07-04 22:35:13 +0530

    adding rssfeeds

--- vulgar/settings.py
+++ vulgar/settings.py
@@ -112,6 +112,7 @@ if USE_S3:
     AWS_S3_REGION_NAME = "ap-south-1"
     AWS_S3_SIGNATURE_VERSION = "s3v4"
     AWS_S3_ADDRESSING_STYLE = "path"
+    SECURE_SSL_REDIRECT = True
     # DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 else:
     MEDIA_URL = '/media/'
