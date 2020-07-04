46a313cf2fc53bd982fe273b0db162dc6005a467
shawakash54 <akashshaw@ACKO398.local>
2020-07-04 22:40:40 +0530

    adding rssfeeds

--- vulgar/feed.py
+++ vulgar/feed.py
@@ -16,6 +16,9 @@ class PoliticsPostsFeed(Feed):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='politics', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
 
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/politics/'
+
     def link(self, obj):
         return f'https://trikonindia.com/politics/'
 
@@ -37,6 +40,9 @@ class TechnologyPostsFeed(Feed):
     def items(self):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='technology', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
+    
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/technology/'
 
     def link(self, obj):
         return f'https://trikonindia.com/technology/'
@@ -52,13 +58,16 @@ class TechnologyPostsFeed(Feed):
 
 
 class SportsPostsFeed(Feed):
-    title = "sports"
+    title = "Sports"
     # link = "/sports/"
     description = "Sports News - Read Latest Sports News Today Headlines on TrikonIndia.com. Find latest cricket news, tennis, football, hockey, World cup 2019, IPL 2020 Live Score Updates. Stay updated on Sports News. Get West Bengal sports updates. Get India sports updates. | TrikonIndia"
 
     def items(self):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='sports', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
+
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/sports/'
 
     def link(self, obj):
         return f'https://trikonindia.com/sports/'
@@ -74,13 +83,16 @@ class SportsPostsFeed(Feed):
 
 
 class HealthPostsFeed(Feed):
-    title = "health"
+    title = "Health"
     # link = "/health/"
     description = "Medical news and health news headlines posted throughout the day, every day - India, West Bengal. | TrikonIndia"
 
     def items(self):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='health', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
+
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/health/'
 
     def link(self, obj):
         return f'https://trikonindia.com/health/'
@@ -96,13 +108,16 @@ class HealthPostsFeed(Feed):
 
 
 class EconomyPostsFeed(Feed):
-    title = "economy"
+    title = "Economy"
     # link = "/economy/"
     description = "Business News - Read Latest Financial news, Stock/Share Market News, Economy News, Business News. Find IPO Analysis, Mutual Funds Trends & Analysis, Gold Rate, Real Estate & more. | TrikonIndia"
 
     def items(self):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='economy', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
+
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/economy/'
 
     def link(self, obj):
         return f'https://trikonindia.com/economy/'
@@ -118,13 +133,16 @@ class EconomyPostsFeed(Feed):
 
 
 class EntertainmentPostsFeed(Feed):
-    title = "entertainment"
+    title = "Entertainment"
     # link = "/entertainment/"
     description = "Latest entertainment news and gossip from the world of bollywood, Hollywood and regional film industries. Get the latest celebrity news on celebrity scandals, engagements, and divorces. Get latest updates on nepotism, inside knowledge of bollywood and hollywood industry. | TrikonIndia"
 
     def items(self):
         return BlogLanguage.published_objects.filter(blog__primary_category__slug='entertainment', \
             created_at__gte=datetime.now(tz=get_current_timezone())-timedelta(days=5))
+
+    def feed_url(self, obj):
+        return f'https://trikonindia.com/feed/entertainment/'
 
     def link(self, obj):
         return f'https://trikonindia.com/entertainment/'
